0d63ba7e5327f04061bf40e111978f48323ac1c1
siarheiMishota <Cthutq..1996..Vbijnf>
2020-10-16 23:28:53 +0300

    start commit

---
+++ src/main/java/com/epam/esm/entity/Tag.java
@@ -0,0 +1,61 @@
+package com.epam.esm.entity;
+
+public class Tag {
+    private long id;
+    private String name;
+
+    public Tag() {
+    }
+
+    public Tag(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        if (!super.equals(object)) return false;
+
+        Tag tag = (Tag) object;
+
+        if (id != tag.id) return false;
+        return name != null ? name.equals(tag.name) : tag.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (int) (id ^ (id >>> 32));
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder()
+                .append("Tag( id= ")
+                .append(id)
+                .append(", name= ")
+                .append(name)
+                .append(");")
+                .toString();
+    }
+}

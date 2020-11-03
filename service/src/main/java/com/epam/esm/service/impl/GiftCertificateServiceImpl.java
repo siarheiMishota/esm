fe21e22817dd755312831b91f842e35121a8bdf3
siarheiMishota <Cthutq..1996..Vbijnf>
2020-11-03 12:44:25 +0300

    added doc

--- service/src/main/java/com/epam/esm/service/impl/GiftCertificateServiceImpl.java
+++ service/src/main/java/com/epam/esm/service/impl/GiftCertificateServiceImpl.java
@@ -25,6 +25,19 @@ public class GiftCertificateServiceImpl implements GiftCertificateService {
         this.tagService = tagService;
     }
 
+    /**
+     *
+     * @param parameters - Map of parameters which are searched for certificates.
+     *  Kind of parameters:
+     *      "name,searchingName" - where name - the field name will be name which will be search on, searchingName -
+     *      value which will be search on.
+     *      "description,searchingDescription" - the field name will be description which will be search
+     *          on, searchingDescription - value which will be search on.
+     *      "sort,name:asc" - where sort means that sorting will be performed, name - the field name will be sort,
+     *          asc or desc - sets the sorting direction. ":asc",":desc" - optional parameter, if they are missing the
+     *          default value will be used ":asc".
+     * @return List of GiftCertificates which match the parameters
+     */
     @Override
     public List<GiftCertificate> findAll(Map<String, String> parameters) {
         return giftCertificateDao.findAll(parameters);
